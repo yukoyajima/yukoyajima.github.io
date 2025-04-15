f835f23fffcc0dff2b905453be016249f43cb6d7
yukoyajima <yukoyaji23@gmail.com>
2025-04-14 23:45:25 -0700

    adding popup

---
+++ modal.js
@@ -0,0 +1,7 @@
+function openModal(id) {
+    document.getElementById(id).style.display = 'block';
+  }
+  
+  function closeModal(id) {
+    document.getElementById(id).style.display = 'none';
+  }
